138f3ed7d7fb7848305af5e2ec32881f8c9f862d
Paul M. Nannery <kc2vrj@gmail.com>
2024-06-02 19:56:49 -0400

    Initialized Firebase Admin app and set up database URL.

--- src/firebase.js
+++ src/firebase.js
@@ -1,29 +1,6 @@
-const firebase = require('firebase/compat/app');
-require('firebase/compat/firestore');
+import { initializeApp } from 'firebase-admin/app';
 
-const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-};
-
-Object.keys(firebaseConfig).forEach(key => {
-  if (!firebaseConfig[key]) {
-    console.warn(`Environment variable ${key} is not defined`);
-  }
+initializeApp({
+  credential: applicationDefault(),
+  databaseURL: 'https://strat-notes-app-default-rtdb.firebaseio.com'
 });
-
-firebase.initializeApp(firebaseConfig);
-
-const db = firebase.firestore();
-const firebaseApp = firebase.app();
-
-// Create references to the 'techs' and 'sites' collections
-const techsCollection = db.collection('techs');
-const sitesCollection = db.collection('sites');
-
-module.exports = { db, firebaseApp, techsCollection, sitesCollection };
