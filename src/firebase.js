a3602a2e8082bfa4506eceb5c0608329aa3ca378
Paul M. Nannery <kc2vrj@gmail.com>
2024-06-02 11:20:26 -0400

    Updated Firebase configuration and created references to 'techs' and 'sites' collections.

--- src/firebase.js
+++ src/firebase.js
@@ -1,23 +1,21 @@
-// firebase.js
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/firestore';
 
-// Firebase configuration
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: `${process.env.FIREBASE_PROJECT_ID}.firebaseapp.com`,
-  databaseURL: `https://strat-notes-app-default-rtdb.firebaseio.com/`,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
   projectId: process.env.FIREBASE_PROJECT_ID,
   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
-  
 
 firebase.initializeApp(firebaseConfig);
 
 export const db = firebase.firestore();
 export const firebaseApp = firebase.app();
+
+// Create references to the 'techs' and 'sites' collections
 export const techsCollection = db.collection('techs');
 export const sitesCollection = db.collection('sites');
