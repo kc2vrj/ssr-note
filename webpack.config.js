85ebcae046b41881e9530b55671b9bbbe51cd734
Paul M. Nannery <kc2vrj@gmail.com>
2024-06-02 20:26:45 -0400

    Updated webpack config to disable fallback for specific modules.

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,14 @@
-
-const path = require('path');
-const webpack = require('webpack');
-
 module.exports = {
-  resolve: {
-    fallback: {
-      "crypto": require.resolve("crypto-browserify"),
-      "stream": require.resolve("stream-browserify"),
-      "url": require.resolve("url/"),
-      "util": require.resolve("util/"),
-      "assert": require.resolve("assert/"),
-      "http": require.resolve("stream-http"),
-      "https": require.resolve("https-browserify"),
-      "os": require.resolve("os-browserify/browser"),
-      "path": require.resolve("path-browserify"),
-      "querystring": require.resolve("querystring-es3"),
-      "zlib": require.resolve("browserify-zlib"),
-      "fs": require.resolve("browserify-fs"),
-      "child_process": false,
-      "net": require.resolve("net-browserify"),
-      "tls": require.resolve("tls-browserify")
-    }
-  },
-  plugins: [
-    new webpack.ProvidePlugin({
-      process: 'process/browser',
-      Buffer: ['buffer', 'Buffer'],
-    }),
-  ],
-};
+    // ...
+    resolve: {
+      fallback: {
+        "util": false,
+        "stream": false,
+        "crypto": false,
+        "url": false,
+        "assert": false,
+        "net": false,
+        "tls": false,
+        "fs": false 
+      }
+    }};
