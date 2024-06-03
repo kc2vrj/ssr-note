913c64b910fe3cc0f32c3f9ac9d81bcabafa14cd
Paul M. Nannery <kc2vrj@gmail.com>
2024-06-03 08:04:40 -0400

    Refactor App component to use BrowserRouter and add key prop to list items.

--- src/App.js
+++ src/App.js
@@ -1,40 +1,42 @@
 import React from 'react';
-import { Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import NoteForm from './components/NoteForm';
 import NoteList from './components/NoteList';
 import AdminPage from './components/AdminPage';
 
 const App = ({ initialData, ...props }) => {
   return (
-    <div className="container">
-      <h1>
-        <img 
-          src="https://stratfiresecurity.com/wp-content/uploads/2021/11/logo.png" 
-          alt="Logo" 
-          style={{ height: "50px" }} 
-        /> 
-        Note Taking App
-      </h1>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Note Entry</Link>
-          </li>
-          <li>
-            <Link to="/notes">View Notes</Link>
-          </li>
-          <li>
-            <Link to="/admin">Admin Page</Link>
-          </li>
-        </ul>
-      </nav>
+    <Router>
+      <div className="container">
+        <h1>
+          <img 
+            src="https://stratfiresecurity.com/wp-content/uploads/2021/11/logo.png" 
+            alt="Logo" 
+            style={{ height: "50px" }} 
+          /> 
+          Note Taking App
+        </h1>
+        <nav>
+          <ul>
+            <li key="note-entry">
+              <Link to="/">Note Entry</Link>
+            </li>
+            <li key="view-notes">
+              <Link to="/notes">View Notes</Link>
+            </li>
+            <li key="admin-page">
+              <Link to="/admin">Admin Page</Link>
+            </li>
+          </ul>
+        </nav>
 
-      <Routes>
-        <Route path="/" element={<NoteForm sites={props.sites} {...props} />} />
-        <Route path="/notes" element={<NoteList sites={props.sites} {...props} />} />
-        <Route path="/admin" element={<AdminPage {...props} />} />
-      </Routes>
-    </div>
+        <Routes>
+          <Route path="/" element={<NoteForm sites={props.sites} {...props} />} />
+          <Route path="/notes" element={<NoteList sites={props.sites} {...props} />} />
+          <Route path="/admin" element={<AdminPage {...props} />} />
+        </Routes>
+      </div>
+    </Router>
   );
 };
 
