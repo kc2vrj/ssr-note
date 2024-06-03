c248649c13e1fc7467acd6919a71d1a412166b3a
Paul M. Nannery <kc2vrj@gmail.com>
2024-06-02 20:34:32 -0400

    Refactor server.js to render the App component with context and remove unnecessary code.

--- src/server.js
+++ src/server.js
@@ -4,37 +4,30 @@ import React from 'react';
 import ReactDOMServer from 'react-dom/server';
 import { StaticRouter } from 'react-router-dom/server';
 import fs from 'fs';
-import AdminPage from './components/AdminPage';
+import App from './App';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.resolve(__dirname, '../build')));
 
-app.get('/admin', (req, res) => {
+app.get('*', (req, res) => {
+  const context = {};
   const reactComponent = ReactDOMServer.renderToString(
-    <StaticRouter location={req.url}>
-      <AdminPage />
+    <StaticRouter location={req.url} context={context}>
+      <App />
     </StaticRouter>
   );
 
-  const indexFile = path.join(__dirname, 'build', 'index.html');
+  const indexFile = path.resolve(__dirname, '../build/index.html');
   fs.readFile(indexFile, 'utf8', (err, data) => {
     if (err) {
       console.error('Something went wrong:', err);
       return res.status(500).send('Oops, better luck next time!');
     }
 
-    const propsScript = `
-      <script>
-        window.__INITIAL_PROPS__ = ${JSON.stringify({ /* Add props here */ })}
-      </script>
-    `;
-
     return res.send(
-      data
-        .replace('<div id="root"></div>', `<div id="root">${reactComponent}</div>`)
-        .replace('<!--PROPS_SCRIPT-->', propsScript)
+      data.replace('<div id="root"></div>', `<div id="root">${reactComponent}</div>`)
     );
   });
 });
