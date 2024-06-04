77beee6f97ad008394b02785bb5e94bfb4bb3a07
Paul M. Nannery <kc2vrj@gmail.com>
2024-06-04 18:25:46 -0400

    Added fallbacks and timers configuration in webpack config.

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,23 @@
 module.exports = {
-    // ...
-    resolve: {
-      fallback: {
-        "util": require.resolve("util/"),
-        "stream": require.resolve("stream-browserify"),
-        "crypto": require.resolve("crypto-browserify"),
-        "url": require.resolve("url/"),
-        "assert": require.resolve("assert/"),
-        "os": require.resolve("os-browserify/browser"),
-        "https": require.resolve("https-browserify"),
-        "querystring": require.resolve("querystring-es3"),
-        "zlib": require.resolve("browserify-zlib"),
-        "timers": require.resolve("timers-browserify"),
-        "http": require.resolve("stream-http"),
-        "fs": require.resolve("fs-browser") // Use the polyfill
-        // Add any other required fallbacks
-      }
+  // ...
+  resolve: {
+    fallback: {
+      "util": require.resolve("util/"),
+      "stream": require.resolve("stream-browserify"),
+      "crypto": require.resolve("crypto-browserify"),
+      "url": require.resolve("url/"),
+      "assert": require.resolve("assert/"),
+      "os": require.resolve("os-browserify/browser"),
+      "https": require.resolve("https-browserify"),
+      "querystring": require.resolve("querystring-es3"),
+      "zlib": require.resolve("browserify-zlib"),
+      "timers": require.resolve("timers-browserify"),
+      "http": require.resolve("stream-http"),
+      "fs": require.resolve("fs-browser") // Use the polyfill
+      // Add any other required fallbacks
     }
+  },
+  node: {
+    timers: ['setTimeout', 'clearTimeout', 'setInterval', 'clearInterval']
   }
+};
